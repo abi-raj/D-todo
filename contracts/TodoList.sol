9c6bad0e6b89a9dd858f2ec83956402084844c3e
abiraj-rajendran <abiraj.rajendran@zoho.com>
2021-10-22 23:17:03 +0530

    static files

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,6 +3,22 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract TodoList{
     uint public tasksCount=0; //number of tasks
-    
+
+    struct Task{
+       uint id; 
+       string content;
+       bool completed;
+    }
+
+    mapping(uint => Task) public tasks; //mapping of tasks
+
+constructor() public{
+    createTask("gf");
+}
+
+    function createTask(string memory _content)public  {
+        tasksCount++; 
+        tasks[tasksCount]  = Task(tasksCount,_content, false);
+    }
 
 }
